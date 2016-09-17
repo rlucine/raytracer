a46f5f581ee9f3cf4ddc33237f0d77b57a0cad26
Rena <renashinomiya@gmail.com>
2016-09-17 16:12:12 -0500

    Update shape struct definitions

--- src/shape.h
+++ src/shape.h
@@ -12,34 +12,72 @@
 #include "vector.h"
 
 /**********************************************************//**
- * @typedef SHAPE_KEY
+ * @typedef SHAPE_TYPE
  * @brief Integer representing every kind of shape
  **************************************************************/
 typedef enum {
-    SHAPE_NONE = 0,
-    SHAPE_SPHERE,
-    SHAPE_ELLIPSOID,
-} SHAPE_KEY;
+    SHAPE_NONE = 0,     ///< The shape is not initialized
+    SHAPE_SPHERE,       ///< The shape is a sphere
+    SHAPE_ELLIPSOID,    ///< The shape is an ellipsoid
+} SHAPE_TYPE;
 
 /**********************************************************//**
- * @typedef SHAPE_DATA
- * @brief Union of shape-specific data
+ * @typedef SPHERE
+ * @brief Struct for storing sphere data
  **************************************************************/
-typedef union {
-    double radius;      ///< Radius of a sphere
-    VECTOR dimension;   ///< Dimensions of an ellipsoid
-} SHAPE_DATA;
+typedef struct {
+    POINT center;   ///< The center of the sphere
+    double radius;  ///< The radius of the sphere
+} SPHERE;
+
+/**********************************************************//**
+ * @typedef ELLIPSOID
+ * @brief Struct for storing ellipsoid data
+ **************************************************************/
+typedef struct {
+    POINT center;       ///< The center of the ellipsoid
+    VECTOR dimension;   ///< The dimensions of the ellipsoid
+} ELLIPSOID;
+
+/**********************************************************//**
+ * @typedef MATERIAL
+ * @brief Struct for storing object material data
+ **************************************************************/
+typedef struct {
+    RGB color;      ///< The color of the object
+} MATERIAL;
 
 /**********************************************************//**
  * @typedef SHAPE
  * @brief Polymorphic struct for storing shape data
+ * SHAPE is not responsible for allocating or freeing the data
+ * field, as this field may also be stack-allocated.
  **************************************************************/
 typedef struct {
-    SHAPE_KEY shape;    ///< Shape key for which field of the union is initialized
-    RGB color;          ///< Color of this shape
-    POINT center;       ///< Location of this shape
-    SHAPE_DATA data;    ///< Polymorphic shape data
+    SHAPE_TYPE shape;       ///< Key representing the type of geometry
+    void *data;             ///< Pointer to the shape geometry
+    MATERIAL material;      ///< The material of the object
 } SHAPE;
+
+/**********************************************************//**
+ * @typedef COLLISION_TYPE
+ * @brief Integer representing what type of collision occurred
+ **************************************************************/
+typedef enum {
+    COLLISION_NONE = 0,     ///< No collision occurred
+    COLLISION_INSIDE,       ///< The point is inside the shape
+    COLLISION_SURFACE,      ///< The point is on the surface
+} COLLISION_TYPE;
+
+/**********************************************************//**
+ * @typedef COLLISION
+ * @brief Data about the collision with a shape
+ **************************************************************/
+typedef struct {
+    COLLISION_TYPE how; ///< How the collision occurred
+    VECTOR where;       ///< Point of collision on the surface
+    double distance;    ///< How far away the point is
+} COLLISION;
 
 /*============================================================*/
 #endif // _SHAPE_H_
