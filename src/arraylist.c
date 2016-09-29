de7daa79308990da954ec7bc997a130aeca0139d
Rena <renashinomiya@gmail.com>
2016-09-28 19:31:51 -0500

    Add initial arraylist module

---
+++ src/arraylist.c
@@ -0,0 +1,141 @@
+/**********************************************************//**
+ * @file arraylist.c
+ * @brief Header file for ARRAYLIST data structure
+ * @author Alec Shinomiya
+ **************************************************************/
+
+// Standard library
+#include <stdlib.h>     // size_t, malloc, free
+#include <stdio.h>      // stderr, fprintf
+#include <string.h>     // memcpy
+
+// This project
+#include "macro.h"
+#include "arraylist.h"
+
+/*============================================================*
+ * Creation function
+ *============================================================*/
+int arraylist_Create(ARRAYLIST *list, size_t itemsize, size_t maxlength) {
+    // Error checking
+    if (itemsize <= 0) {
+#ifdef VERBOSE
+        fprintf(stderr, "arraylist_Create failed: Invalid item size\n");
+#endif
+        return FAILURE;
+    }
+    if (maxlength <= 0) {
+#ifdef VERBOSE
+        fprintf(stderr, "arraylist_Create failed: Invalid capacity\n");
+#endif
+        return FAILURE;
+    }
+    
+    // Set up properties
+    list->maxlength = maxlength;
+    list->itemsize = itemsize;
+    list->length = 0;
+    
+    // Set up data array
+    list->data = malloc(itemsize * maxlength);
+    if (!list->data) {
+#ifdef VERBOSE
+        fprintf(stderr, "arraylist_Create failed: Out of memory\n");
+#endif
+        return FAILURE;
+    }
+    return SUCCESS;
+}
+
+/*============================================================*
+ * Getter
+ *============================================================*/
+size_t arraylist_Length(const ARRAYLIST *list) {
+    return list->length;
+}
+
+/*============================================================*
+ * Item getter
+ *============================================================*/
+void *arraylist_At(ARRAYLIST *list, int index) {
+    // Perform pointer arithmetic
+    char *temp = list->data;
+    if (sizeof(char) != 1) {
+#ifdef VERBOSE
+        fprintf(stderr, "arraylist_At failed: sizeof(char) is %d on this system\n", sizeof(char));
+#endif
+        return NULL;
+    }
+    
+    temp += index * list->itemsize;
+    return (void *)temp;
+}
+
+/*============================================================*
+ * Appending function
+ *============================================================*/
+int arraylist_Append(ARRAYLIST *list, const void *item) {
+    // Grow the arraylist if it is too small
+    if (list->length >= list->maxlength) {
+        void *new = malloc(list->itemsize * list->maxlength * 2);
+        if (!new) {
+#ifdef VERBOSE
+            fprintf(stderr, "arraylist_Append failed: Out of memory\n");
+#endif
+            return FAILURE;
+        }
+        memcpy(new, list->data, list->length * list->itemsize);
+        free(list->data);
+        list->data = new;
+    }
+    
+    // Copy the new item into the arraylist
+    void *destination = arraylist_At(list, list->length);
+    if (!destination) {
+#ifdef VERBOSE
+        fprintf(stderr, "arraylist_Append failed: Unable to offset into data field\n");
+#endif
+        return FAILURE;
+    }
+    memcpy(destination, item, list->itemsize);
+    list->length++;
+    return SUCCESS;
+}
+
+/*============================================================*
+ * Suze reduction
+ *============================================================*/
+int arraylist_Compress(ARRAYLIST *list) {
+    // Tries to reduce size usage
+    void *new = malloc(list->itemsize * list->length);
+    if (!new) {
+#ifdef VERBOSE
+        fprintf(stderr, "arraylist_Compress failed: Out of memory\n");
+#endif
+        return FAILURE;
+    }
+    memcpy(new, list->data, list->length * list->itemsize);
+    free(list->data);
+    list->data = new;
+    return SUCCESS;
+}
+
+/*============================================================*
+ * Convert to normal array
+ *============================================================*/
+void *arraylist_GetData(ARRAYLIST *list) {
+    // Get all the data out
+    return list->data;
+}
+
+/*============================================================*
+ * Destructor
+ *============================================================*/
+void arraylist_Destroy(ARRAYLIST *list) {
+    if (list->data) {
+        free(list->data);
+    }
+    return;
+}
+
+/*============================================================*/
---
+++ src/arraylist.h
@@ -0,0 +1,87 @@
+/**********************************************************//**
+ * @file arraylist.h
+ * @brief Header file for ARRAYLIST data structure
+ * @author Alec Shinomiya
+ **************************************************************/
+
+#ifndef _ARRAYLIST_H_
+#define _ARRAYLIST_H_
+
+// Standard library
+#include <stdlib.h>     // size_t
+
+// This project
+#include "macro.h"
+
+/**********************************************************//**
+ * @struct ARRAYLIST
+ * @brief Struct containing variable length array of any
+ * data type.
+ **************************************************************/
+typedef struct {
+    size_t maxlength;   ///< The actual size of the data block in item units
+    size_t length;      ///< The number of items in the list
+    size_t itemsize;    ///< The size of each item unit
+    void *data;         ///< The actual data in the list
+} ARRAYLIST;
+
+/**********************************************************//**
+ * @brief Create an empty arraylist with the given capacity
+ * @param list: The arraylist to initialize
+ * @param itemsize: The size of each element of the arraylist
+ * @param maxlength: The capacity of the arraylist
+ * @return SUCCESS or FAILURE
+ **************************************************************/
+extern int arraylist_Create(ARRAYLIST *list, size_t itemsize, size_t maxlength);
+
+/**********************************************************//**
+ * @brief Get the number of items in the arraylist
+ * @param list: The arraylist to check
+ * @return The number of items in the list
+ **************************************************************/
+extern size_t arraylist_Length(const ARRAYLIST *list);
+
+/**********************************************************//**
+ * @brief Get a pointer to the given item in the arraylist
+ * @param list: The arraylist to check
+ * @param index: The index of the item from 0 to the length-1
+ * @return Pointer to the item or NULL on failure
+ **************************************************************/
+extern void *arraylist_At(ARRAYLIST *list, int index);
+
+/**********************************************************//**
+ * @brief Append the new item to the end of the arraylist.
+ * This function may grow the arraylist.
+ * @param list: The arraylist to append to
+ * @param item: The new data to copy in to the list. The data
+ * can be deleted afterward, although this will not perform
+ * a deep copy of any allocated regions.
+ * @return SUCCESS or FAILURE
+ **************************************************************/
+extern int arraylist_Append(ARRAYLIST *list, const void *item);
+
+/**********************************************************//**
+ * @brief Reduce the capacity of the arraylist to the length
+ * in order to save memory usage.
+ * @param list: The arraylist to compress
+ * @return SUCCESS or FAILURE
+ **************************************************************/
+extern int arraylist_Compress(ARRAYLIST *list);
+
+/**********************************************************//**
+ * @brief Convert the arraylist to a normal array. If the
+ * ARRAYLIST is deleted the return value of this function will
+ * be freed.
+ * @param list: The arraylist to read
+ * @return Pointer to the data array or NULL on failure
+ **************************************************************/
+extern void *arraylist_GetData(ARRAYLIST *list);
+
+/**********************************************************//**
+ * @brief Destroy the arraylist and free allocated data.
+ * @param list: The arraylist to destroy
+ **************************************************************/
+extern void arraylist_Destroy(ARRAYLIST *list);
+
+/*============================================================*/
+#endif // _ARRAYLIST_H_
